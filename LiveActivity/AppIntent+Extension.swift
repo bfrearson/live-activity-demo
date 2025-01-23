31aec435b363ee8683f5c40ba653ece110f00c37
Ben Frearson <57793137+bfrearson@users.noreply.github.com>
2025-01-23 12:31:05 +0000

    remove dependencies from widget extension

--- LiveActivity/AppIntent+Extension.swift
+++ LiveActivity/AppIntent+Extension.swift
@@ -8,7 +8,7 @@
 import AppIntents
 
 // Placeholder to allow widget extension to compile.
-// Per Apple's docs, `LiveActivityIntent` is onyl ever called from the main app
+// Per Apple's docs, `LiveActivityIntent` is only ever called from the main app
 // so this will never actually be called
  extension PlayPauseIntent: LiveActivityIntent, @unchecked Sendable {
     func perform() async throws -> some IntentResult {
