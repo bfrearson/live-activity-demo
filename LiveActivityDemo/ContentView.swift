18cfbcc9426ed1553bd6fd7690d169389db255b4
Ben Frearson <57793137+bfrearson@users.noreply.github.com>
2025-01-23 09:22:27 +0000

    Initial Commit

---
+++ LiveActivityDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveActivityDemo
+//
+//  Created by Ben Frearson on 23/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiveActivityDemo/LiveActivityDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityDemoApp.swift
+//  LiveActivityDemo
+//
+//  Created by Ben Frearson on 23/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivityDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
